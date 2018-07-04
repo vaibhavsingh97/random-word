75376e263057857a9ab7817b937363a322d95423
Jatin <jatinrohilla69@gmail.com>
2018-07-04 21:54:30 +0530

    updated unit tests

--- random_word/tests/test_word.py
+++ random_word/tests/test_word.py
@@ -2,18 +2,18 @@ from unittest import TestCase, main
 from random_word import RandomWords
 
 
-class RandomWordTest(TestCase):
-    def set_up(self):
-        r = RandomWords()
+class TestRandomWord(TestCase):
+	# using single instance for all tests
+    r = RandomWords()
 
-        def random_word(self):
-            return r.get_random_word()
+    def test_random_word(self):
+        return self.r.get_random_word()
 
-        def random_words(self):
-            return r.get_random_words()
+    def test_random_words(self):
+        return self.r.get_random_words()
 
-        def word_of_the_day(self):
-            return r.word_of_the_day()
+    def test_word_of_the_day(self):
+        return self.r.word_of_the_day()
 
 
 if __name__ == '__main__':
