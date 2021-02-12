d26038a51051e09f4d741357cba9a3ee50ba757b
Vaibhav Singh <singh.vaibhav2011@gmail.com>
2021-02-13 02:15:30 +0530

    Fix: descrypt script and build file

--- .github/scripts/decrypt_secrets.sh
+++ .github/scripts/decrypt_secrets.sh
@@ -4,5 +4,7 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
+echo 'Directory structure'
+ls -la
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output $HOME/secrets/config.yml random_word/config.yml.gpgs
+--output $HOME/secrets/config.yml config.yml.gpgs
