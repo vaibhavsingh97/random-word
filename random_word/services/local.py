d86a8e3eb4dcc9fe8ec12ed68d6c052e8b11fb30
Vaibhav Singh <singh.vaibhav2011@gmail.com>
2024-11-16 19:14:59 -0500

    chore: fix file loading for every function call

--- random_word/services/local.py
+++ random_word/services/local.py
@@ -6,8 +6,8 @@ from os.path import abspath, dirname, join
 class Local(object):
     def __init__(self):
         self.source = join(dirname(__file__), "..", "database", "words.json")
+        with open(self.source) as word_database:
+            self.valid_words = load(word_database)
 
     def get_random_word(self):
-        with open(self.source) as word_database:
-            valid_words = load(word_database)
-        return secrets.choice(list(valid_words.keys()))
+        return secrets.choice(list(self.valid_words.keys()))
